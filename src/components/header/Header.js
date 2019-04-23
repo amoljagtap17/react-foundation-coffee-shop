b778adb180180547d6c88981ede04730bbec3cf1
Amol Jagtap <amoljagtap17@gmail.com>
2019-04-23 13:24:19 +0530

    REACT_FOUNDATION_COFFEE_SHOP: Add Header component with styles

---
+++ src/components/header/Header.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import headerStyles from 'components/header/header.module.scss'
+import logoImg from 'assets/img/logo.svg'
+
+const Header = () => (
+  <header className={headerStyles.siteHeader}>
+    <div className="container">
+      <div className={headerStyles.headerContent}>
+        <div className={headerStyles.topSection}>
+          <div className={headerStyles.logo}>
+            <img src={logoImg} alt="logo" />
+          </div>
+          <div className={headerStyles.menus}>
+            <nav className={headerStyles.socialMenu}>
+              <a href="https://www.facebook.com">Facebook</a>
+              <a href="https://www.twitter.com">Twitter</a>
+              <a href="https://www.instagram.com">Instagram</a>
+              <a href="https://www.youtube.com">Youtube</a>
+            </nav>
+
+            <div className="title-bar" data-responsive-toggle="main-nav" data-hide-for="medium">
+              <button className="menu-icon" type="button" data-toggle="main-nav"></button>
+              <div className="title-bar-title">Menu</div>
+            </div>
+
+            <ul id="main-nav" className={headerStyles.mainNavigation}>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/">About Us</Link>
+              </li>
+              <li>
+                <Link to="/">Process</Link>
+              </li>
+              <li>
+                <Link to="/">Menu</Link>
+              </li>
+              <li>
+                <Link to="/">Gallery</Link>
+              </li>
+              <li>
+                <Link to="/">Contact Us</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={headerStyles.bottomSection}>
+          <h1 className={headerStyles.slogan}>A new Experience Everyday</h1>
+        </div>
+      </div>
+    </div>
+  </header>
+)
+
+export default Header
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import $ from 'jquery'
+import 'foundation-sites'
+
+import Header from 'components/header/Header'
 
 import Home from 'pages/Home'
 
-const Routes = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-    </Switch>
-  </BrowserRouter>
-)
+class Routes extends Component {
+
+  componentDidMount() {
+    $(document).foundation()
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter >
+    )
+  }
+}
+
+export default Routes
